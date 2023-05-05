f988b49ccff1a526e0bcc4ec7193af43cd8b985f
Maninder <maninder.123.456@live.com>
2023-05-04 20:59:11 -0400

    refactored nav bar and index/landing, and css

---
+++ js/navbar.js
@@ -0,0 +1,29 @@
+// function resizeIframe() {
+//   const iframe = document.getElementById('iframe')
+//   iframe.height = iframe.contentWindow.document.body.scrollHeight + 50 + "px";
+// }
+// // (windows).on("resize", resizeIframe)
+// window.addEventListener('resize', resizeIframe);
+
+// This loads the nav stuff for each page
+//   $.get("../navStuff.html", function (data) {
+//     $("#nav-stuff-placeholder").replaceWith(data);
+//   });
+console.log("hello world");
+// This loads the content for each page
+$.get("/html/landing.html", function (data) {
+    $("#page-content-placeholder").replaceWith(data);
+});
+
+function navItemOnClick(element, itemName) {
+
+    var element = document.getElementById(itemName + '-li')
+    if (!element.classList.contains('current-selection')) {
+        document.getElementById('about-li').classList.remove("current-selection");
+        document.getElementById('work-li').classList.remove("current-selection");
+        document.getElementById('projects-li').classList.remove("current-selection");
+        element.classList.add("current-selection");
+    }
+
+
+}
